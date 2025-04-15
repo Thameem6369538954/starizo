015e7928426020538f392dd0f6f844a105145ac0
Thameem Ansari <thameemrpp@gmail.com>
2025-04-15 18:43:24 +0530

    mmmm

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -36,18 +36,18 @@ const Header = () => {
       style={{ backgroundImage: `url(${BG})` }}
     >
       {/* Content */}
-      <div className="relative z-10 flex flex-col md:flex-row items-center justify-between gap-8 md:gap-12 w-full max-w-7xl mx-auto p-6">
+      <div className="relative z-10 flex flex-col md:flex-row items-center justify-between gap-8 md:gap-12 w-full max-w-7xl mx-auto p-6 mt-50 md:mt-25 lg:mt-0">
         {/* Left - Text */}
         <div className="text-white text-center md:text-left space-y-4 md:space-y-6">
           <motion.h1
-            className="text-4xl sm:text-3xl md:text-6xl lg:text-8xl font-[heading] text-[#00a63e]"
+            className="text-6xl sm:text-5xl md:text-7xl lg:text-8xl font-[heading] text-[#00a63e]"
             custom={1}
             initial="hidden"
             animate="visible"
             variants={textVariants}
           >
-            <span className="text-[#00a63e] text-9xl">H</span>arnessing Rice{" "}
-            <span className="text-yellow-400 text-9xl">S</span><span className="text-yellow-400">ustainably</span>
+            <span className="text-[#00a63e] text-6xl sm:text-9xl">H</span>arnessing Rice{" "}
+            <span className="text-yellow-400 text-6xl sm:text-9xl">S</span><span className="text-yellow-400">ustainably</span>
           </motion.h1>
 
           <motion.p
